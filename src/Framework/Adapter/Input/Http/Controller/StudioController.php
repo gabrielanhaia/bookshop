3663cd8440c1f0f8239351818e719817b30feb33
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2024-07-07 23:06:35 +0200

    Add usecase to controler

--- src/Framework/Adapter/Input/Http/Controller/StudioController.php
+++ src/Framework/Adapter/Input/Http/Controller/StudioController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Framework\Adapter\Input\Http\Controller;
 
+use App\Application\Port\Input\RegisterNewStudio\RegisterNewStudioPort;
+use App\Application\Port\Input\RegisterNewStudio\StudioDTO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,9 +14,34 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class StudioController extends AbstractController
 {
-    #[Route('/studio', name: 'studio', methods: [Request::METHOD_POST])]
-    public function handle(): Response
+    public function __construct(
+        private readonly RegisterNewStudioPort $registerNewStudioPort
+    )
     {
-        return new JsonResponse('Studio created', Response::HTTP_CREATED);
+    }
+
+    #[Route('/studio', name: 'studio', methods: [Request::METHOD_POST])]
+    public function handle(Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $studioDTO = new StudioDTO(
+            name: $data['name'],
+            street: $data['street'],
+            city: $data['city'],
+            zipCode: $data['zipCode'],
+            country: $data['country'],
+            email: $data['email']
+        );
+        $studioDTO = $this->registerNewStudioPort->registerNewStudio($studioDTO);
+
+        return new JsonResponse([
+            'id' => $studioDTO->getId(),
+            'name' => $studioDTO->getName(),
+            'street' => $studioDTO->getStreet(),
+            'city' => $studioDTO->getCity(),
+            'zipCode' => $studioDTO->getZipCode(),
+            'country' => $studioDTO->getCountry(),
+            'email' => $studioDTO->getEmail()
+        ], Response::HTTP_CREATED);
     }
 }
